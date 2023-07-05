86f2d9d910c4f68583ae92fe3ec2f67693d5fa9a
“Caroline <carolinefabri.dev@hotmail.com>
2023-07-05 12:07:40 +0200

    not done yet

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
-
 const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 
@@ -13,10 +12,49 @@ app.use(expressLayouts);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Add the route handlers here:
-
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beers => {
+      res.render('beers', { beers });
+    })
+    .catch(error => {
+      console.log(error);
+      res.send('Error retrieving beers');
+    });
+});
+
+app.get('/beers/:id', (req, res) => {
+  const beerId = req.params.id;
+
+  punkAPI
+    .getBeer(beerId)
+    .then(beer => {
+      res.render('beer-details', { beer });
+    })
+    .catch(error => {
+      console.log(error);
+      res.send('Error retrieving beer details');
+    });
+});
+
+app.get('/random-beer', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(responseFromAPI => {
+      const randomBeer = responseFromAPI[0];
+      console.log('Random beer from the database:', randomBeer);
+      res.render('random-beer', { beer: randomBeer });
+   
+    })
+    .catch(error => {
+      console.error(error);
+      res.send('An error occurred while retrieving a random beer.');
+    });
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
